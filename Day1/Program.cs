f90c7036b41b68d533f133bb8f4d50e5336692e8
j.sunkler <j.sunkler@orgacard.de>
2022-12-07 14:03:38 +0100

    completed day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -9,14 +9,36 @@ string textPath = Path.Combine(assemblyDirectory, "input.debug.txt");
 string textPath = Path.Combine(assemblyDirectory, "input.txt");
 #endif
 
-string[] input = File.ReadAllLines(textPath);
+string input = File.ReadAllText(textPath);
 
 // Part 1
 
 int counter = 0;
 
-foreach (string line in input)
+foreach (char c in input)
 {
+    if (c == '(') counter++;
+    if (c == ')') counter--;
+
 }
 
 Console.WriteLine($"Teil 1: {counter}");
+
+// Part 2
+counter = 0;
+
+for (int i = 1; i <= input.Length; i++)
+{
+    char c = input[i-1];
+
+    if (c == '(') counter++;
+    if (c == ')')
+    {
+        counter--;
+        if (counter < 0)
+        {
+            Console.WriteLine($"Teil 2: {i}");
+            break;
+        }
+    }
+}
